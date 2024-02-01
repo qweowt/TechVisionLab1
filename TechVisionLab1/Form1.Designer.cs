30f079bad1f66d0f38994a2f53219d7de4ed0284
qweowt <klimenkonikita81@gmail.com>
2024-02-01 21:20:34 +0300

    Добавьте файлы проекта.

---
+++ TechVisionLab1/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace TechVisionLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            OpenFile = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            SaveFile = new Button();
+            saveFileDialog1 = new SaveFileDialog();
+            CountPointTrackBar = new TrackBar();
+            CountPointLabel = new Label();
+            PointGeneration = new Button();
+            dataGridView1 = new DataGridView();
+            X = new DataGridViewTextBoxColumn();
+            Y = new DataGridViewTextBoxColumn();
+            Z = new DataGridViewTextBoxColumn();
+            ShowData = new Button();
+            pictureBox1 = new PictureBox();
+            Draw = new Button();
+            DrawPeriod = new Button();
+            textBox1 = new TextBox();
+            Task4 = new Button();
+            Clusters = new Button();
+            ((System.ComponentModel.ISupportInitialize)CountPointTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(185, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(636, 64);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // OpenFile
+            // 
+            OpenFile.Location = new Point(12, 12);
+            OpenFile.Name = "OpenFile";
+            OpenFile.Size = new Size(167, 29);
+            OpenFile.TabIndex = 1;
+            OpenFile.Text = "Open File";
+            OpenFile.UseVisualStyleBackColor = true;
+            OpenFile.Click += OpenFile_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // SaveFile
+            // 
+            SaveFile.Location = new Point(12, 47);
+            SaveFile.Name = "SaveFile";
+            SaveFile.Size = new Size(167, 29);
+            SaveFile.TabIndex = 2;
+            SaveFile.Text = "Save File";
+            SaveFile.UseVisualStyleBackColor = true;
+            SaveFile.Click += SaveFile_Click;
+            // 
+            // CountPointTrackBar
+            // 
+            CountPointTrackBar.AllowDrop = true;
+            CountPointTrackBar.Location = new Point(185, 82);
+            CountPointTrackBar.Maximum = 1024;
+            CountPointTrackBar.Name = "CountPointTrackBar";
+            CountPointTrackBar.Size = new Size(613, 56);
+            CountPointTrackBar.TabIndex = 4;
+            CountPointTrackBar.Scroll += CountPointTrackBar_Scroll;
+            // 
+            // CountPointLabel
+            // 
+            CountPointLabel.AutoSize = true;
+            CountPointLabel.Location = new Point(789, 91);
+            CountPointLabel.Name = "CountPointLabel";
+            CountPointLabel.Size = new Size(17, 20);
+            CountPointLabel.TabIndex = 5;
+            CountPointLabel.Text = "0";
+            // 
+            // PointGeneration
+            // 
+            PointGeneration.Location = new Point(12, 82);
+            PointGeneration.Name = "PointGeneration";
+            PointGeneration.Size = new Size(167, 29);
+            PointGeneration.TabIndex = 6;
+            PointGeneration.Text = "Point Generation";
+            PointGeneration.UseVisualStyleBackColor = true;
+            PointGeneration.Click += PointGeneration_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { X, Y, Z });
+            dataGridView1.Location = new Point(12, 164);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(403, 391);
+            dataGridView1.TabIndex = 7;
+            // 
+            // X
+            // 
+            X.HeaderText = "X";
+            X.MinimumWidth = 6;
+            X.Name = "X";
+            X.Width = 125;
+            // 
+            // Y
+            // 
+            Y.HeaderText = "Y";
+            Y.MinimumWidth = 6;
+            Y.Name = "Y";
+            Y.Width = 125;
+            // 
+            // Z
+            // 
+            Z.HeaderText = "Z";
+            Z.MinimumWidth = 6;
+            Z.Name = "Z";
+            Z.Width = 125;
+            // 
+            // ShowData
+            // 
+            ShowData.Location = new Point(12, 117);
+            ShowData.Name = "ShowData";
+            ShowData.Size = new Size(403, 29);
+            ShowData.TabIndex = 8;
+            ShowData.Text = "Show Data";
+            ShowData.UseVisualStyleBackColor = true;
+            ShowData.Click += DataGridShow;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(421, 155);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(400, 400);
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // Draw
+            // 
+            Draw.Location = new Point(421, 117);
+            Draw.Name = "Draw";
+            Draw.Size = new Size(400, 29);
+            Draw.TabIndex = 10;
+            Draw.Text = "Draw";
+            Draw.UseVisualStyleBackColor = true;
+            Draw.Click += Draw_Click;
+            // 
+            // DrawPeriod
+            // 
+            DrawPeriod.Location = new Point(827, 47);
+            DrawPeriod.Name = "DrawPeriod";
+            DrawPeriod.Size = new Size(161, 29);
+            DrawPeriod.TabIndex = 11;
+            DrawPeriod.Text = "Draw Period";
+            DrawPeriod.UseVisualStyleBackColor = true;
+            DrawPeriod.Click += DrawPeriod_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(827, 14);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(161, 27);
+            textBox1.TabIndex = 12;
+            // 
+            // Task4
+            // 
+            Task4.Location = new Point(827, 82);
+            Task4.Name = "Task4";
+            Task4.Size = new Size(161, 29);
+            Task4.TabIndex = 13;
+            Task4.Text = "Task4";
+            Task4.UseVisualStyleBackColor = true;
+            Task4.Click += Task4_Click;
+            // 
+            // Clusters
+            // 
+            Clusters.Location = new Point(827, 117);
+            Clusters.Name = "Clusters";
+            Clusters.Size = new Size(161, 29);
+            Clusters.TabIndex = 14;
+            Clusters.Text = "Clusters";
+            Clusters.UseVisualStyleBackColor = true;
+            Clusters.Click += Clusters_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(999, 561);
+            Controls.Add(Clusters);
+            Controls.Add(Task4);
+            Controls.Add(textBox1);
+            Controls.Add(DrawPeriod);
+            Controls.Add(Draw);
+            Controls.Add(pictureBox1);
+            Controls.Add(ShowData);
+            Controls.Add(dataGridView1);
+            Controls.Add(PointGeneration);
+            Controls.Add(CountPointLabel);
+            Controls.Add(CountPointTrackBar);
+            Controls.Add(SaveFile);
+            Controls.Add(OpenFile);
+            Controls.Add(richTextBox1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form1";
+            Text = "Lab1Klimenko";
+            ((System.ComponentModel.ISupportInitialize)CountPointTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button OpenFile;
+        private OpenFileDialog openFileDialog1;
+        private Button SaveFile;
+        private SaveFileDialog saveFileDialog1;
+        private TrackBar CountPointTrackBar;
+        private Label CountPointLabel;
+        private Button PointGeneration;
+        private DataGridView dataGridView1;
+        private Button ShowData;
+        private DataGridViewTextBoxColumn X;
+        private DataGridViewTextBoxColumn Y;
+        private DataGridViewTextBoxColumn Z;
+        private PictureBox pictureBox1;
+        private Button Draw;
+        private Button DrawPeriod;
+        private TextBox textBox1;
+        private Button Task4;
+        private Button Clusters;
+    }
+}
---
+++ TechVisionLab1/Form1.cs
@@ -0,0 +1,228 @@
+using System.Data;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace TechVisionLab1
+{
+    public partial class Form1 : Form
+    {
+        List<int> PointsX = new List<int>();
+        List<int> PointsY = new List<int>();
+        List<int> PointsZ = new List<int>();
+        int CountPoints = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            Clear();
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            string fileText = System.IO.File.ReadAllText(filename);
+            richTextBox1.Text = fileText;
+
+            FindCountPoints();
+
+            DataToList();
+        }
+
+        private void FindCountPoints()
+        {
+            string DataStr = richTextBox1.Text;
+            DataStr = DataStr.Remove(DataStr.Length - 1);
+            int[] ints = Array.ConvertAll(DataStr.Split(), int.Parse);
+            CountPoints = ints.Length / 3;
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog1.FileName;
+            System.IO.File.WriteAllText(filename, richTextBox1.Text);
+        }
+
+        private void CountPointTrackBar_Scroll(object sender, EventArgs e)
+        {
+            CountPointLabel.Text = (CountPointTrackBar.Value).ToString();
+            CountPoints = CountPointTrackBar.Value;
+        }
+
+        private void PointGeneration_Click(object sender, EventArgs e)
+        {
+            Clear();
+            int X, Y, Z;
+            Random random = new Random();
+            for (int i = 0; i < CountPoints; i++)
+            {
+                X = random.Next(1023);
+                Y = random.Next(1023);
+                Z = random.Next(1023);
+
+                richTextBox1.Text += $"{X} {Y} {Z} ";
+            }
+            DataToList();
+        }
+
+        private void Clear()
+        {
+            richTextBox1.Clear();
+            PointsX.Clear();
+            PointsY.Clear();
+            PointsZ.Clear();
+            dataGridView1.Rows.Clear();
+            pictureBox1.Image = null;
+        }
+
+        private void DataToList()
+        {
+            string DataStr = richTextBox1.Text;
+            DataStr = DataStr.Remove(DataStr.Length - 1);
+            int[] ints = Array.ConvertAll(DataStr.Split(), int.Parse);
+            int helpI = 1;
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (helpI == 1)
+                {
+                    PointsX.Add(ints[i]);
+                    helpI++;
+                }
+                else if (helpI == 2)
+                {
+                    PointsY.Add(ints[i]);
+                    helpI++;
+                }
+                else if (helpI == 3)
+                {
+                    PointsZ.Add(ints[i] / 4);
+                    helpI = 1;
+                }
+
+            }
+        }
+
+        private void DataGridShow(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = true;
+
+            dataGridView1.AllowUserToAddRows = false;
+
+            for (int i = 0; i < CountPoints; i++)
+            {
+                dataGridView1.Rows.Add(PointsX[i], PointsY[i]);
+            }
+
+            for (int i = 0; i < CountPoints; i++)
+            {
+                dataGridView1.Rows[i].Cells[2].Style.BackColor = Color.FromArgb(255, 0, 0, PointsZ[i]);
+            }
+        }
+
+        private void Draw_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            for (int i = 0; i < CountPoints; i++)
+            {
+                Color PointColor = Color.FromArgb(255, 0, PointsZ[i], 0);
+                Brush brush = new SolidBrush(PointColor);
+                g.FillEllipse(brush, (int)(PointsX[i] / 2.56), (int)(PointsY[i] / 2.56), 3, 3);
+            }
+        }
+
+        private void DrawPeriod_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            int cordY = -20;
+            int period = int.Parse(textBox1.Text);
+            for (int i = 0; i < CountPoints; i++)
+            {
+                if (i % period == 0)
+                    cordY += 21;
+                if (i % period < period)
+                {
+                    Color PointColor = Color.FromArgb(255, 0, 0, PointsX[i] / 4);
+                    Brush brush = new SolidBrush(PointColor);
+                    g.FillRectangle(brush, (int)(PointsX[i] / 2.56), cordY, 5, 20);
+                }
+            }
+        }
+
+        private void Task4_Click(object sender, EventArgs e)
+        {
+            double sum1 = 0, sum2 = 0, sum3 = 0;
+            double sum4 = 0;
+
+            for (int i = 0; i < CountPoints; i++)
+            {
+                sum1 += PointsX[i] * PointsY[i];
+                sum2 += PointsX[i];
+                sum3 += PointsY[i];
+                sum4 += Math.Pow(PointsX[i], 2);
+            }
+            double A = ((CountPoints * sum1 - sum2) / (sum4 - Math.Pow(sum2, 2)));
+            double B = ((sum3 - A * sum2) / CountPoints);
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            for (int i = 0; i < CountPoints; i++)
+            {
+                double y = (A * PointsX[i] + B);
+                Brush brush1 = new SolidBrush(Color.Red);
+                g.FillEllipse(brush1, (int)(PointsX[i] / 2.56), (int)(y / 2.56), 5, 5);
+
+                Color PointColor = Color.FromArgb(255, 0, PointsZ[i], 0);
+                Brush brush = new SolidBrush(PointColor);
+                g.FillEllipse(brush, (int)(PointsX[i] / 2.56), (int)(PointsY[i] / 2.56), 3, 3);
+            }
+        }
+
+        private void Clusters_Click(object sender, EventArgs e)
+        {
+            List<int> Clusters = new List<int>();
+            List<int> cordX = new List<int>();
+            List<int> cordY = new List<int>();
+            for (int i = 0; i < CountPoints; i++)
+            {
+                int PointsInCluster = 0;
+                for (int j = 0; j < CountPoints; j++)
+                {
+                    double x = Math.Sqrt(Math.Pow(PointsX[j] - PointsX[i], 2) + Math.Pow(PointsY[j] - PointsY[i], 2));
+
+                    if (x <= 45)
+                    {
+                        PointsInCluster++;
+                    }  
+                }
+                if (PointsInCluster > CountPoints / 85.3)
+                {
+                    Clusters.Add(PointsInCluster);
+                    cordX.Add(PointsX[i]);
+                    cordY.Add(PointsY[i]);
+                }
+            }
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            for (int i = 0; i < CountPoints; i++)
+            {
+                Color PointColor = Color.FromArgb(255, 0, PointsZ[i], 0);
+                Brush brush = new SolidBrush(PointColor);
+                g.FillEllipse(brush, (int)(PointsX[i] / 2.56), (int)(PointsY[i] / 2.56), 5, 5);
+            }
+
+            for (int i = 0;i < Clusters.Count;i++)
+            {
+                Color PointColor = Color.Red;
+                Pen pen = new Pen(PointColor);
+                g.DrawEllipse(pen, (int)(cordX[i]/2.56-20), (int)(cordY[i] / 2.56 - 20), 45, 45);
+            }
+        }
+    }
+}
